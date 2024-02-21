e6d2726361452bd9f951ca7f1612c1c238658f85
KIM SO JIN <138649793+NinNiNanNa@users.noreply.github.com>
2024-02-21 20:06:26 +0900

    #1 CHAT WITH WEBSOCKETS

--- zoom3/src/public/js/app.js
+++ zoom3/src/public/js/app.js
@@ -3,6 +3,13 @@ const nickForm = document.querySelector("#nick");
 const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`);
 
+// type은 메세지의 종류, payload는 메세지에 담겨있는 정보
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  // JSON을 String으로 변환해서 return
+  return JSON.stringify(msg);
+}
+
 socket.addEventListener("open", () => {
   console.log("Connected to Server ✅");
 });
@@ -30,15 +37,23 @@ messageForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const input = messageForm.querySelector("input");
   // console.log(input.value);
-  socket.send(input.value);
+  // socket.send(input.value);
+  socket.send(makeMessage("new_message", input.value));
+  const li = document.createElement("li");
+  li.innerText = `You: ${input.value}`;
+  messageList.append(li);
   input.value = "";
 });
 
 nickForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const input = nickForm.querySelector("input");
+  /*
   socket.send({
     type: "nickname",
     payload: input.value,
   });
+  */
+  socket.send(makeMessage("nickname", input.value));
+  input.value = "";
 });
--- zoom3/src/server.js
+++ zoom3/src/server.js
@@ -1,7 +1,6 @@
 import http from "http";
 import WebSocket from "ws";
 import express from "express";
-import { type } from "os";
 
 const app = express();
 
@@ -21,20 +20,44 @@ const wss = new WebSocket.Server({ server });
 const sockets = [];
 
 wss.on("connection", (socket) => {
+  // console.log(socket);
   // 연결된 브라우저를 sockets 배열에 저장
   sockets.push(socket);
+  socket["nickname"] = "Anon";
   console.log("Connected to Browser ✅");
 
   socket.on("close", () => {
     console.log("Disconnected from the Browser ❌");
   });
 
-  socket.on("message", (message) => {
-    // console.log(message.toString());
-    // socket.send(message.toString());
+  socket.on("message", (msg) => {
+    // console.log(msg.toString());
+    // socket.send(msg.toString());
 
+    // String인 msg를 javascript Object로 변환
+    const message = JSON.parse(msg);
+    // console.log(message, msg.toString());
+    // if문 switch문으로 정리
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+        break;
+      case "nickname":
+        // console.log(message.payload);
+        socket["nickname"] = message.payload;
+        break;
+    }
+    /*
+    if (message.type === "new_message") {
+      sockets.forEach((aSocket) => aSocket.send(message.payload));
+    } else if (message.type === "nickname") {
+      console.log(message.payload);
+    }
+    */
     // 각 브라우저를 aSocket으로 표시하고 메세지를 보낸다.
-    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+    // sockets.forEach((aSocket) => aSocket.send(msg.toString()));
   });
 });
 
